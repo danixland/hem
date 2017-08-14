619829ebb7a8e1eb3db3704a4dba490bdcc2a307
danixland <danix@danixland.net>
2017-08-14 21:58:02 +0200

    undefined constants. fixed error.

--- config.php
+++ config.php
@@ -8,13 +8,13 @@
 /**
 * Please provide your MySQL login information below.
 */
-define(DB_HOST, "localhost");
-define(DB_USER, "test");
-define(DB_PASS, "test");
-define(DB_NAME, "test");
+define("DB_HOST", "localhost");
+define("DB_USER", "test");
+define("DB_PASS", "test");
+define("DB_NAME", "test");
 
 
-define(ACCOUNT_TYPES, array(
+define("ACCOUNT_TYPES", array(
 	'bank' => 0,
 	'credit card' => 1,
 	'prepaid card' => 2
